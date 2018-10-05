8d4e55457bf3ca388382b55dc69407c586951d96
ffiamz <ffiamz@163.com>
2018-10-05 21:56:35 +0800

    repair login

--- share/header.php
+++ share/header.php
@@ -2,12 +2,13 @@
 echo "<div id='header'><ul>";
 session_start();
 if(isset($_SESSION['student_login_id'])){
-	echo "<li><a href='/sems/student/student_login.html'>登陆</a></li>
-		<li><a href='/sems/student/student_add.html'>注册</a></li>";
-}else{
-	echo "<li><a href='/sems/student/login.php?action=\"logout\"'>
+	echo "<li><a href='/sems/student/login.php?action=logout'>
 		退出登陆
 		</a></li>";
+
+}else{
+	echo "<li><a href='/sems/student/student_login.html'>登陆</a></li>
+		<li><a href='/sems/student/student_add.html'>注册</a></li>";
 }
 
 echo "<li><a href='/sems/course/main.html'>课程</a></li>
