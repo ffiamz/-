1e6fbb8755c793c79e07d68c6dafe776df993604
ffiamz <ffiamz@163.com>
2018-10-05 13:43:24 +0800

    change capatity to capacity

--- course/scripts/addCourse.php
+++ course/scripts/addCourse.php
@@ -6,7 +6,7 @@
     $teacherName = $_POST['teacherName'];
     $courseCredit = $_POST['courseCredit'];
     $courseClass = $_POST['courseClass'];
-    $capatity = $_POST['capatity'];
+    $capacity = $_POST['capacity'];
     */
     $courseId = $_POST['courseId'];
     $courseName = $_POST['courseName'];
@@ -15,10 +15,10 @@
     $teacherGender = $_POST['teacherGender'];
     $courseCredit = $_POST['courseCredit'];
     $courseClass = $_POST['courseClass'];
-    $capatity = $_POST['capatity'];
+    $capacity = $_POST['capacity'];
     
     if($courseId=='' || $courseName=='' || $teacherId=='' || $teacherName=='' || $teacherGender=='' ||
-    $courseClass=='' || $courseCredit=='' || $capatity==''){
+    $courseClass=='' || $courseCredit=='' || $capacity==''){
         echo "不能有空字段!<br>";
         return;
     }
@@ -40,8 +40,8 @@
     */
     
     $sql = "INSERT IGNORE INTO
-    tb_course(CourseId, CourseName, CourseCredit, CourseClass, Capatity, TeacherId, Remain)
-    VALUES(".$courseId.", '".$courseName."', ".$courseCredit.", ".$courseClass.", ".$capatity.", ".$teacherId.", ".$capatity.");";
+    tb_course(CourseId, CourseName, CourseCredit, CourseClass, Capacity, TeacherId, Remain)
+    VALUES(".$courseId.", '".$courseName."', ".$courseCredit.", ".$courseClass.", ".$capacity.", ".$teacherId.", ".$capacity.");";
     if($conn->query($sql)==False){
         echo "插入失败！<br>". $conn->error;
         $conn->close();
