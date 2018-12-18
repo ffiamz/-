699922eb3fba15adf17c1378313670aa0d6cd47f
wc <2362465844Qqq.com>
2018-12-19 01:31:14 +0800

    add year and month

--- blog_system/model/templatetags/blog_tags.py
+++ blog_system/model/templatetags/blog_tags.py
@@ -22,12 +22,22 @@ def archives():
 @register.simple_tag
 def get_categories():
     # Count 计算分类下的文章数，其接受的参数为需要技术的模型的名称
+    cate = Category.objects.all()
+    for c in cate:
+        if c.blog_set.count() == 0:
+            c.delete()
+
     return Category.objects.annotate(num_posts=Count('blog')).filter(num_posts__gt=0)
 
 
 # 获取标签列表
 @register.simple_tag
 def get_tags():
+    tags = Tag.objects.all()
+    for t in tags:
+        if t.blog_set.count() == 0:
+            t.delete()
+
     return Tag.objects.annotate(num_posts=Count('blog')).filter(num_posts__gt=0)
 
 
